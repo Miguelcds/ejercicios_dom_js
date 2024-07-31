290643e6584526367cc510c20da7da19d7b8d52d
Joao Miguel Costa Da Silva <miguel-cds@hotmail.com>
2024-07-31 22:23:01 +0200

    1

--- ejercicio1/index.js
+++ ejercicio1/index.js
@@ -1,34 +1,41 @@
 // 1.1 Usa querySelector para mostrar por consola el botón con la clase .showme
 
-const button = document.querySelector("button")
+const button = document.querySelector(".showme")
 
 console.log(button);
 
 
 // 1.2 Usa querySelector para mostrar por consola el h1 con el id #pillado
 
+const h1 = document.querySelector("#pillado")
 
-
+console.log(h1);
 
 // 1.3 Usa querySelector para mostrar por consola todos los p
 
+const allP = document.querySelectorAll("p")
+
+console.log(allP);
 
 
-
-// 1.4 Usa querySelector para mostrar por consola todos los elementos con 
+// 1.4 Usa querySelector para mostrar por consola todos los elementos con
 // 	la clase.pokemon
 
+const pokemon = document.querySelectorAll(".pokemon")
 
+console.log(pokemon);
 
-
-
-// 1.5 Usa querySelector para mostrar por consola todos los elementos con 
+// 1.5 Usa querySelector para mostrar por consola todos los elementos con
 // el atributo data-function="testMe".
 
+const classData = document.querySelectorAll(`[data-function="testMe"]`)
+
+console.log(classData);
 
 
-
-
-
-// 1.6 Usa querySelector para mostrar por consola el 3 personaje con el atributo 
+// 1.6 Usa querySelector para mostrar por consola el 3 personaje con el atributo
 // data-function="testMe".
+
+
+console.log(classData[2]);
+
